f350f41243380ed1a9e853d2c83a325c3a5a99f0
comus3 <come.plantin.carrenard@gmail.com>
2023-02-27 11:08:25 +0100

    commit de test pour baptiste temporaire

--- test_utils.py
+++ test_utils.py
@@ -5,6 +5,8 @@ import math
 def test_fact():
     assert utils.fact(5) == 120
     assert utils.fact(6) == 720
+    for i in dico:
+        assert utils.fact(dico[i]) == dico[i].key 
 
 
 def test_roots():
@@ -12,5 +14,5 @@ def test_roots():
 
 def test_integrate():
     assert abs(utils.integrate("x*((math.e)**x)",0,1)-(((2*((math.e)**3)+1))/9))<0.001
-    
+
 
